223143b378d275bfde18d80585155abc7c15a62c
Dev Steve <wearypossum4770@yahoo.com>
2024-07-21 01:48:17 -0400

    setup fleek

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  exportPathMap: function(){
+    return {
+      '/': { page: '/'}
+    }
+  },
   trailingSlash: true,
 }
 
---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About(){
+    return (
+<main>
+<h1>About Page</h1>
+<p>Under construction</p>
+</main>
+    )
+}
