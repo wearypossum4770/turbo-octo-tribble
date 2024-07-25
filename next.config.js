5812645317323df2de23059c720107c7beb93e6d
Dev Steve <wearypossum4770@yahoo.com>
2024-07-25 19:38:57 -0400

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
-  output: 'export',
+  generateBuildId: async () => process.env.GIT_HASH,
+  output: 'standalone',
   reactStrictMode: true,
+  crossOrigin: 'anonymous',
   images: {
     unoptimized: true,
+  },
+  devIndicators: {
+    buildActivityPosition: 'bottom-right',
   },
   trailingSlash: true,
 }
